100e404a2182eaecbf26e8584e301d49d85fbc16
vaipatel <vai.pateln@gmail.com>
2021-08-13 08:04:40 -0400

    Try render to texture (not working)

--- FluidSimWindow.cpp
+++ FluidSimWindow.cpp
@@ -5,7 +5,7 @@
 #include <QWindow>
 
 FluidSimWindow::FluidSimWindow(QWindow* _parent) : OpenGLWindow(_parent),
-    m_program(nullptr),
+    m_targetFBOProgram(nullptr),
     m_frame(0)
 {
 
@@ -13,13 +13,23 @@ FluidSimWindow::FluidSimWindow(QWindow* _parent) : OpenGLWindow(_parent),
 
 void FluidSimWindow::initialize()
 {
-    m_program = new QOpenGLShaderProgram(this);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSource);
-    m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSource);
-    m_program->link();
-    m_posAttr = static_cast<GLuint>(m_program->attributeLocation("posAttr"));
-    m_colAttr = static_cast<GLuint>(m_program->attributeLocation("colAttr"));
-    m_matrixUniform = static_cast<GLuint>(m_program->uniformLocation("matrix"));
+    m_targetFBOProgram = new QOpenGLShaderProgram(this);
+    m_targetFBOProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, targetFBOVertexShaderSource);
+    m_targetFBOProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, targetFBOFragmentShaderSource);
+    m_targetFBOProgram->link();
+    m_posAttr = static_cast<GLuint>(m_targetFBOProgram->attributeLocation("posAttr"));
+    m_colAttr = static_cast<GLuint>(m_targetFBOProgram->attributeLocation("colAttr"));
+    m_matrixUniform = static_cast<GLuint>(m_targetFBOProgram->uniformLocation("matrix"));
+
+    m_screenProgram = new QOpenGLShaderProgram(this);
+    m_screenProgram->addShaderFromSourceCode(QOpenGLShader::Vertex, screenVertexShaderSource);
+    m_screenProgram->addShaderFromSourceCode(QOpenGLShader::Fragment, screenFragmentShaderSource);
+    bool canLinkScreenProg = m_screenProgram->link();
+    if ( !canLinkScreenProg )
+    {
+        exit(1);
+    }
+    m_screenTextureHandle = static_cast<GLuint>(m_screenProgram->uniformLocation("screenTexture"));
 }
 
 void FluidSimWindow::render()
@@ -34,7 +44,7 @@ void FluidSimWindow::render()
     GLsizei viewWidth = width() * retinaScale;
     GLsizei viewHeight = height() * retinaScale;
 
-    glViewport(0, 0, viewWidth * retinaScale, viewHeight * retinaScale);
+    glViewport(0, 0, viewWidth, viewHeight);
 
     //
     // 1. The framebuffer is our container for our target texture
@@ -77,17 +87,112 @@ void FluidSimWindow::render()
     // Always check that our framebuffer is ok
     {
         bool frameBufferStatusOk = glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE;
-        assert(frameBufferStatusOk);
+        if ( !frameBufferStatusOk )
+        {
+            qDebug() << "VAIVAI" << "FrameBuffer status not ok";
+            exit(1);
+        }
+    }
+    // Unbind here and give back to default FBO. Is this just for good practice here? I'll have to rebind to targetFBO now.
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    //
+    // 4. First Pass
+    //
+    // -----
+    // first pass
+    glBindFramebuffer(GL_FRAMEBUFFER, targetFBO);
+    glClearColor(1.f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // we're not using the stencil buffer now
+//    glEnable(GL_DEPTH_TEST);
+    DrawTargetFBO();
+
+    //
+    // 5. Second Pass
+    //
+    // -----
+    // Unbind here and give back to default FBO
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glViewport(0, 0, viewWidth, viewHeight);
+
+
+    bool couldBindShader = m_screenProgram->bind();
+    if ( !couldBindShader )
+    {
+        qDebug() << "VAIVAI" << "Could not bind screen shader";
+        exit(1);
     }
 
-    bool couldBindShader = m_program->bind();
+//    m_screenProgram->setUniformValue(static_cast<int>(m_screenTextureHandle), targetTexture);
+
+    GLuint quadVAO = 0;
+    glGenVertexArrays(1, &quadVAO);
+    glBindVertexArray(quadVAO);
+
+//    static const GLfloat g_quad_vertex_buffer_data[] = {
+//        -1.0f, -1.0f, 0.0f,
+//        1.0f, -1.0f, 0.0f,
+//        -1.0f,  1.0f, 0.0f,
+//        -1.0f,  1.0f, 0.0f,
+//        1.0f, -1.0f, 0.0f,
+//        1.0f,  1.0f, 0.0f,
+//    };
+
+//    GLuint quad_vertexbuffer;
+//    glGenBuffers(1, &quad_vertexbuffer);
+//    glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(g_quad_vertex_buffer_data), g_quad_vertex_buffer_data, GL_STATIC_DRAW);
+
+    float quadVertices[] = {
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+
+        -1.0f,  1.0f,  0.0f, 1.0f,
+         1.0f, -1.0f,  1.0f, 0.0f,
+         1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    GLuint quadVerticesbuffer;
+    glGenBuffers(1, &quadVerticesbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVerticesbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(quadVAO);
+    glDisable(GL_DEPTH_TEST);
+    glBindTexture(GL_TEXTURE_2D, targetTexture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    m_screenProgram->release();
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+    glDeleteFramebuffers(1, &targetFBO);
+
+    ++m_frame;
+//    if (m_frame >= 2)
+//    {
+//        m_frame = 0;
+    //    }
+}
+
+void FluidSimWindow::DrawTargetFBO()
+{
+    bool couldBindShader = m_targetFBOProgram->bind();
     assert(couldBindShader);
 
     QMatrix4x4 matrix;
     matrix.perspective(60.0f, 4.0f/3.0f, 0.1f, 100.0f);
     matrix.translate(0, 0, -2);
-//    matrix.rotate(static_cast<float>(100.0 * m_frame / screen()->refreshRate()), 0, 1, 0);
-    m_program->setUniformValue(static_cast<int>(m_matrixUniform), matrix);
+    matrix.rotate(static_cast<float>(100.0 * m_frame / screen()->refreshRate()), 0, 1, 0);
+    m_targetFBOProgram->setUniformValue(static_cast<int>(m_matrixUniform), matrix);
 
     GLfloat vertices[] = {
         0.0f, 0.707f,
@@ -104,21 +209,13 @@ void FluidSimWindow::render()
     glVertexAttribPointer(m_posAttr, 2, GL_FLOAT, GL_FALSE, 0, vertices);
     glVertexAttribPointer(m_colAttr, 3, GL_FLOAT, GL_FALSE, 0, colors);
 
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(m_posAttr);
+    glEnableVertexAttribArray(m_colAttr);
 
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    glDisableVertexAttribArray(1);
-    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(m_posAttr);
+    glDisableVertexAttribArray(m_colAttr);
 
-    glDeleteFramebuffers(1, &targetFBO);
-
-    m_program->release();
-
-//    ++m_frame;
-//    if (m_frame >= 2)
-//    {
-//        m_frame = 0;
-//    }
+    m_targetFBOProgram->release();
 }
--- FluidSimWindow.h
+++ FluidSimWindow.h
@@ -19,23 +19,68 @@ private:
     GLuint m_colAttr;
     GLuint m_matrixUniform;
 
-    const QString vertexShaderSource =
-        "attribute highp vec4 posAttr;\n"
-        "attribute lowp vec4 colAttr;\n"
-        "varying lowp vec4 col;\n"
+    const QString targetFBOVertexShaderSource =
+            "#version 330 core\n"
+        "in highp vec4 posAttr;\n"
+        "in lowp vec4 colAttr;\n"
+        "out lowp vec4 col;\n"
         "uniform highp mat4 matrix;\n"
         "void main() {\n"
         "   col = colAttr;\n"
         "   gl_Position = matrix * posAttr;\n"
         "}\n";
 
-    const QString fragmentShaderSource =
-        "varying lowp vec4 col;\n"
+    const QString targetFBOFragmentShaderSource =
+            "#version 330 core\n"
+        "in vec4 col;\n"
+        "out vec4 FragColor;\n"
         "void main() {\n"
-        "   gl_FragColor = col;\n"
+        "   FragColor = col;\n"
         "}\n";
 
-    QOpenGLShaderProgram* m_program; //!< Need one of these for each step of fluid sim
+    GLuint m_screenTextureHandle;
+
+    const QString screenVertexShaderSource =
+            "#version 330 core\n"
+            "layout (location = 0) in vec2 aPos;\n"
+            "layout (location = 1) in vec2 aTexCoords;\n"
+            "\n"
+            "out vec2 TexCoords;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);\n"
+            "    TexCoords = aTexCoords;\n"
+            "}";
+//            "#version 330 core\n"
+//                "layout (location = 0) in vec3 aPos;\n"
+//                "void main()\n"
+//                "{\n"
+//                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//                "}";
+
+    const QString screenFragmentShaderSource =
+            "#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "in vec2 TexCoords;\n"
+            "uniform sampler2D screenTexture;\n"
+            "\n"
+            "void main()\n"
+            "{\n"
+            "    FragColor = texture(screenTexture, TexCoords);//vec4(TexCoords.x, TexCoords.y, 0, 1);\n"
+            "}";
+//            "#version 330 core\n"
+//        "out vec4 FragColor;\n"
+//        "\n"
+//        "void main()\n"
+//        "{\n"
+//        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+//        "}";
+
+    void DrawTargetFBO();
+
+    QOpenGLShaderProgram* m_targetFBOProgram; //!< Need one of these for each step of fluid sim
+    QOpenGLShaderProgram* m_screenProgram;
     uint m_frame;
 
 };
