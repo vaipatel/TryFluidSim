f8c8b2d0c09157cb9d15ed606fb95ae01ea45c4a
vaipatel <vai.pateln@gmail.com>
2021-09-05 20:08:13 -0400

    Add defines in cpp to switch between experiments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "TriangleRotWindow.h"
 #include "ImageTextureWindow.h"
-
+#include "FluidSimWindow.h"
 #include <QApplication>
+
+#define TRIROT 0
+#define PERLIN 1
+#define FLUID 2
+#define EXPERIMENT_NAME FLUID
+
 
 int main(int argc, char *argv[])
 {
@@ -14,18 +20,28 @@ int main(int argc, char *argv[])
     format.setVersion(4, 2);
     format.setSamples(16);
 
-#if 0
+#if EXPERIMENT_NAME == TRIROT
     TriangleRotWindow triWindow;
     triWindow.setFormat(format);
     triWindow.resize(480, 360);
     triWindow.show();
     triWindow.setAnimating(true);
-#else
+#endif
+
+#if EXPERIMENT_NAME == PERLIN
     ImageTextureWindow imgTexWindow;
     imgTexWindow.setFormat(format);
     imgTexWindow.resize(500, 500);
     imgTexWindow.show();
     imgTexWindow.setAnimating(true);
+#endif
+
+#if EXPERIMENT_NAME == FLUID
+    FluidSimWindow fluidSimWindow;
+    fluidSimWindow.setFormat(format);
+    fluidSimWindow.resize(500, 500);
+    fluidSimWindow.show();
+    fluidSimWindow.setAnimating(true);
 #endif
 
     return app.exec();
